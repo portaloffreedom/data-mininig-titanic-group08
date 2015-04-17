49211a1bd653fa3fc200b322e0df0dc0c1c0539b
Mirkainthecity <bugajmirka8@gmail.com>
2015-04-17 18:26:32 +0200

    Normalization has been done
    blablabla

--- titanic_load_data.R
+++ titanic_load_data.R
@@ -1,4 +1,5 @@
-setwd("/home/matteo/Documenti/VU/Data Mining/Assigment 1/Titanic/")
+#setwd("C:/Users/Mirka/Desktop/datamining/")
+#setwd("/home/matteo/Documenti/VU/Data Mining/Assigment 1/Titanic/")
 data <- read.csv("train.csv", header=TRUE)
 
 ## preprocessing part
@@ -6,10 +7,18 @@ data <- subset(data, select= -PassengerId)
 data <- subset(data, select= -Name)
 data <- subset(data, select= -Cabin)
 data <- subset(data, select= -Ticket)
+data <- subset(data, select= -Embarked)
 
 data$relatives = data$SibSp + data$Parch
 data <- subset(data, select= -SibSp)
 data <- subset(data, select= -Parch)
 
 median_age <- median(data$Age, na.rm = TRUE)
-data$Age[is.na(data$Age)] <- median_age 
+data$Age[is.na(data$Age)] <- median_age
+
+#normalize data
+data$Pclass<-data$Pclass-2
+data$Sex=ifelse(data$Sex=='female', 1,-1)
+data$Age<-(data$Age-mean_all)/sd_all 
+data$Fare<-data$Fare/max(data$Fare)
+data$relatives<-data$relatives/max(data$relatives)
