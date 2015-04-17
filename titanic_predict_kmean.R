6f2f2af8adb051cf214e6359e8aa08949b43bbba
Matteo De Carlo <matteo.dek@gmail.com>
2015-04-17 20:40:27 +0200

    testing over multiple hidden nodes number

--- titanic_predict_kmean.R
+++ titanic_predict_kmean.R
@@ -1,20 +1,28 @@
 source("titanic_predict.R")
 library("RSNNS")
 
-train <- function(data, target) {
-  model <- rbf(data, target,
-               size=3,
-               #maxit=1000,
-               #initFuncParams=c(0, 1, 0, 0.01, 0.01),
-               #learnFuncParams=c(1e-8, 0, 1e-8, 0.1, 0.8), linOut=TRUE
-               )
-  
-  return (model)
-}
-
 fwd <- function(model, data) {
   ret=predict(model,data)
   return(ret)
 }
 
-error = analize(train, fwd)
+errors <- c()
+
+for (i in 2:6) {
+
+  train <- function(data, target) {
+    model <- rbf(data, target,
+                 size=i,
+                 maxit=300,
+                 #initFuncParams=c(0, 1, 0, 0.01, 0.01),
+                 #learnFuncParams=c(1e-8, 0, 1e-8, 0.1, 0.8), linOut=TRUE
+                 )
+    
+    return (model)
+  }
+  
+  error <- analize(train, fwd)
+  errors <- rbind(errors, error)
+
+}
+ 
