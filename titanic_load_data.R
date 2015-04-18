ef1f646fce70d4d562cf21700700c5a415705b92
Matteo De Carlo <matteo.dek@gmail.com>
2015-04-18 17:10:35 +0200

    loadData loads also test data

--- titanic_load_data.R
+++ titanic_load_data.R
@@ -1,7 +1,7 @@
 #setwd("~/GitHub/data-mininig-titanic-group08")
 #setwd("/home/matteo/Documenti/VU/Data Mining/Assigment 1/Titanic/")
 
-loadData <- function(filename) {
+loadData <- function(filename, filename_test = NULL) {
   data <- read.csv(filename, header=TRUE)
   
   ## preprocessing part
@@ -25,8 +25,39 @@ loadData <- function(filename) {
   data$Pclass<-data$Pclass-2
   data$Sex=ifelse(data$Sex=='female', 1,-1)
   data$Age<-(data$Age-mean_age)/sd_age 
-  data$Fare<-data$Fare/max(data$Fare)
-  data$relatives<-data$relatives/max(data$relatives)
   
-  return (data)
+  maxFare <- max(data$Fare)
+  data$Fare<-data$Fare/maxFare
+  
+  maxRelatives <- max(data$relatives)
+  data$relatives<-data$relatives/maxRelatives
+  
+  if (is.null(filename_test)) {
+    return (data)
+  } else {
+    dataTest <- read.csv(filename_test, header=TRUE)
+    
+    ## preprocessing part
+    dataTest <- subset(dataTest, select= -PassengerId)
+    dataTest <- subset(dataTest, select= -Name)
+    dataTest <- subset(dataTest, select= -Cabin)
+    dataTest <- subset(dataTest, select= -Ticket)
+    dataTest <- subset(dataTest, select= -Embarked)
+    
+    dataTest$relatives = dataTest$SibSp + dataTest$Parch
+    dataTest <- subset(dataTest, select= -SibSp)
+    dataTest <- subset(dataTest, select= -Parch)
+    
+    dataTest$Age[is.na(dataTest$Age)] <- median_age
+  
+    dataTest$Pclass<-dataTest$Pclass-2
+    dataTest$Sex=ifelse(dataTest$Sex=='female', 1,-1)
+    dataTest$Age<-(dataTest$Age-mean_age)/sd_age 
+    dataTest$Fare<-dataTest$Fare/maxFare
+    dataTest$Fare[is.na(dataTest$Fare)] <- median(data$Fare)
+    dataTest$relatives<-dataTest$relatives/maxRelatives
+    
+    ret <- c("train data", "test data")
+    return (list(data,dataTest))
+  }
 }
